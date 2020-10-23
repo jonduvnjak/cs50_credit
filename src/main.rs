f87bbcda54218a39a8bcf33c0ead57197b518dd1
jonduvnjak <jon.duvnjak@volumental.com>
2020-10-23 22:22:48 +0200

    Checks credit card numbers

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,76 @@
 fn main() {
-    let cc = vec![1, 2, 3, 4, 5, 5, 6, 6, 7];
+    let cc = vec![4, 0, 0, 3, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4];
 
-    get_every_other_digit(cc);
+    let check_sum = get_every_other_digit(cc);
+    let is_valid = is_cc_valid(check_sum);
+    println!("{}", is_valid);
+    if is_valid == true {
+        println!("Valid card")
+    } else {
+        println!("Not a valid card")
+    }
 }
 
-fn get_every_other_digit(cc: Vec<i32>) -> i32 {
-    let mut cc_iter = cc.iter();
-    let mut a = 0;
-    let mut b = 0;
+fn is_cc_valid(sum: u32) -> bool {
+    if sum % 10 == 0 {
+        true
+    } else {
+        false
+    }
+}
 
-    a += cc_iter.next_back().unwrap();
-    b += cc_iter.next_back().unwrap(); //note - if the number *2 is greater than 10 then I'll need to split that number into its constuent parts
+fn get_every_other_digit(cc: Vec<u32>) -> u32 {
+    let mut cc_iter = cc.iter();
+
+    //Split the vector into two vectors based on the instructions
+    let mut simple = 0;
+    let mut complex = vec![];
+
+    loop {
+        //simple: starting with the last digit, take out each 2nd number and find the total product
+        match cc_iter.next_back() {
+            Some(d) => simple += d,
+            None => break,
+        }
+        //complex: starting with the second to last digit, take out each 2nd number
+        match cc_iter.next_back() {
+            Some(d) => complex.push(d),
+            None => break,
+        };
+    }
+    println!("{}", simple);
+    //complex: multiply each digit of complex by 2
+    let complex = complex.into_iter().map(|x| 2 * x).collect::<Vec<_>>();
+
+    //if any of the digits in complex are > 9 then split that number into individual integers. eg: 14 -> 1 and 4.
+    let split_complex = split_into_int(complex);
+    println!("{:?}", split_complex);
+
+    //take the product of split_complex
+    let split_complex: u32 = split_complex.iter().sum();
+    println!("{:?}", split_complex);
+
+    //take the product of split_complex and the product of simple together
+    let total = split_complex + simple;
 
     //https://cs50.harvard.edu/x/2020/psets/1/credit/
 
-    32
+    total
+}
+
+fn split_into_int(complex: Vec<u32>) -> Vec<u32> {
+    let mut complex_split: Vec<u32> = Vec::new();
+    for i in complex {
+        if i >= 10 {
+            let mut digits: Vec<_> = i
+                .to_string()
+                .chars()
+                .map(|d| d.to_digit(10).unwrap())
+                .collect();
+            complex_split.append(&mut digits);
+        } else {
+            complex_split.push(i);
+        }
+    }
+    complex_split
 }
